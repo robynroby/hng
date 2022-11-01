57dfad47d400a851db1eba64657bfabed97e2bd9
robynroby <robinsonmwaura254@gmail.com>
2022-11-01 17:18:15 +0300

    first commit

---
+++ hngtask/src/assets/index.js
@@ -0,0 +1,8 @@
+import profile from './images/profile.jpg'
+import github from './images/github.png';
+import slack from './images/slack.png';
+import share from './images/share.png';
+import zuri from './images/zuri.png';
+import logo from './images/I4G.png';
+
+export {profile, github, slack, share, zuri, logo};
