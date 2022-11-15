f3ccbae637d63192dd76ad57c77952d30504e826
robynroby <robinsonmwaura254@gmail.com>
2022-11-15 17:46:20 +0300

    fixed some bugs

--- metabnb/src/components/Navbar/Navbar.js
+++ metabnb/src/components/Navbar/Navbar.js
@@ -10,13 +10,23 @@ function Navbar() {
       </div>
       <div className="navbar">
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="/placetostay">Place to stay</a></li>
-          <li><a href="#">NFTs</a></li>
-          <li><a href="#">Community</a></li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="/placetostay">Place to stay</a>
+          </li>
+          <li>
+            <a href="#">NFTs</a>
+          </li>
+          <li>
+            <a href="#">Community</a>
+          </li>
         </ul>
       </div>
-      <div className="btn"><button>Connect wallet</button></div>
+      <div className="btn">
+        <button>Connect wallet</button>
+      </div>
     </div>
   );
 }
